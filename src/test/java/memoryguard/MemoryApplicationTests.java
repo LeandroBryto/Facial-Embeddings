3257b05ea61eaa39fd117531d6ed275492629d35
leandro.brito <leandro.brito@memora.com.brleandro.brito>
2026-03-14 02:44:41 -0300

    test(bootstrap): adicionar teste

---
+++ src/test/java/memoryguard/MemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package memoryguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
