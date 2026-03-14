16daaa604cd346f3b77a28f8d93c3d2c3c626ead
leandro.brito <leandro.brito@memora.com.brleandro.brito>
2026-03-14 02:31:42 -0300

    feat(repo): add sessao biometrica repository

--- src/main/java/memoryguard/repository/SessaoBiometricaRepository.java
+++ src/main/java/memoryguard/repository/SessaoBiometricaRepository.java
@@ -1,5 +1,4 @@
 package memoryguard.repository;
-
 import memoryguard.model.SessaoBiometrica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
