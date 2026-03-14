b4f62663991b98cabaa2aec7255de8340b2c5106
leandro.brito <leandro.brito@memora.com.brleandro.brito>
2026-03-14 02:30:43 -0300

    feat(repo): add perfil usuario repository

---
+++ src/main/java/memoryguard/repository/SessaoBiometricaRepository.java
@@ -0,0 +1,9 @@
+package memoryguard.repository;
+
+import memoryguard.model.SessaoBiometrica;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SessaoBiometricaRepository extends JpaRepository<SessaoBiometrica, Long> {
+}
