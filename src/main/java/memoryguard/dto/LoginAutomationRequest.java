59b39cec3e7d78e92f0e6322490ba89c3947954c
leandro.brito <leandro.brito@memora.com.brleandro.brito>
2026-03-14 02:25:47 -0300

    add login automation request payload

---
+++ src/main/java/memoryguard/dto/LoginAutomationRequest.java
@@ -0,0 +1,11 @@
+package memoryguard.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginAutomationRequest {
+    private Long userId;
+}
