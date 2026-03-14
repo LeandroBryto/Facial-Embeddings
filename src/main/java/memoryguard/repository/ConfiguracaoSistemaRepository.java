91da833290e2373c404ec5057bec283256bd6172
leandro.brito <leandro.brito@memora.com.brleandro.brito>
2026-03-14 02:28:56 -0300

    feat(repo): add configuracao sistema repository

---
+++ src/main/java/memoryguard/repository/ConfiguracaoSistemaRepository.java
@@ -0,0 +1,10 @@
+package memoryguard.repository;
+
+
+import memoryguard.model.ConfiguracaoSistema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfiguracaoSistemaRepository extends JpaRepository<ConfiguracaoSistema, Long> {
+}
